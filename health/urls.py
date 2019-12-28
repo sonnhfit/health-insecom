7776d6992709414d62f93e29207aa8f511dfea59
sonnhfit <sonnhfit@gmail.com>
2019-12-28 11:58:10 +0700

    update

--- health/urls.py
+++ health/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from apps.healthapp import views as health_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('compare-health/', health_views.ComapreHealth.as_view()),
 ]
