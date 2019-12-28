068fed33188d4908bfb1b650e0652fee3153271d
lethi2k <ledinhthi2909@gmail.com>
2019-12-28 11:47:31 +0700

    add compare view

--- apps/healthapp/views.py
+++ apps/healthapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+
+
+class ComapreHealth(View):
+
+    def get(self, request):
+        return render(request,'compare/compare_health.html')
+    
+    def post(self, request):
+        pass
