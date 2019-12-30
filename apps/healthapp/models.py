cc453a4ca143a86cad0db622561164f7c0dca89a
lethi2k <ledinhthi2909@gmail.com>
2019-12-30 13:30:41 +0700

    compare health models

--- apps/healthapp/models.py
+++ apps/healthapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class GetCompareHealth(models.Models):
+    images = models.CharField(max_length=255,blank=false,null=false)
+    product_name = models.TextField(max_length=255,blank=false,null=false)
+    cover = models.IntegerField(default=0)
+    features = models.IntegerField(default=0)
+    premium = models.IntegerField(default=0)
