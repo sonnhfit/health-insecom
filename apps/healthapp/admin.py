f76a9fcc56fcf7e7f46bb35a29116cef133a586e
lethi2k <ledinhthi2909@gmail.com>
2019-12-30 15:43:46 +0700

    show data compare health

--- apps/healthapp/admin.py
+++ apps/healthapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import ProductCompareHealth
 # Register your models here.
+admin.site.register(ProductCompareHealth)
+# admin.site.register(ProductCompareHealth)
