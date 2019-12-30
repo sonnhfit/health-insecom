ce987b3828fcb8384808305b4e2715e62ca2339e
quangchien2682000 <quanglxph07563>
2019-12-30 14:13:04 +0700

    update model prepayment

--- apps/healthapp/models.py
+++ apps/healthapp/models.py
@@ -9,3 +9,13 @@ class ProductCompareHealth(models.Model):
     cover = models.IntegerField(default=0)
     features = models.IntegerField(default=0)
     premium = models.IntegerField(default=0)
+
+
+class PostPrepayment(models.Model): 
+    email = models.CharField(max_length=255)
+    nameUser = models.CharField(max_length=255)
+    fullName = models.CharField(max_length=255)
+    birthDay = models.DateField(auto_now=True)
+    weight = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
+    occupation = models.CharField(max_length=255)
