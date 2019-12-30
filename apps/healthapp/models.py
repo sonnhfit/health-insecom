76b37ece8cdb9199f6277229e01e13201cb82baf
quangchien2682000 <quanglxph07563>
2019-12-30 13:56:01 +0700

    update moder prepayment

--- apps/healthapp/models.py
+++ apps/healthapp/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-class PostPrepayment(models.Model):
-email=models.CharField(max_length=255, blank=false, null=false)
-nameUser=models.CharField(max_length=255, blank=false, null=false)
-fullName=models.CharField(max_length=255, blank=false, null=false)
-birthDay=models.models.DateField(null=false)
-weight=models.IntegerField(default=0)
-height=models.IntegerField(default=0)
-occupation=models.CharField(max_length=255,blank=false,null=false)
+class PostPrepayment(models.Model): 
+    email = models.CharField(max_length=255)
+    nameUser = models.CharField(max_length=255)
+    fullName = models.CharField(max_length=255)
+    birthDay = models.DateField(auto_now=True)
+    weight = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
+    occupation = models.CharField(max_length=255)
 
