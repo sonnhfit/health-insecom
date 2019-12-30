4fadc16ad90ad6f3642dbdb232ba2af502c48ee6
quangchien2682000 <quanglxph07563>
2019-12-30 13:30:12 +0700

    update model prepayment

--- apps/healthapp/models.py
+++ apps/healthapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PostPrepayment(models.Model);
+
+email=models.CharField(max_length=200,blank=False,null=False)
+nameUser=models.CharField(max_length=200,blank=False,null=False)
+fullName=models.CharField(max_length=200,blank=False,null=False)
+birthDay=models.models.DateField(blank=False,null=False)
+weight=models.IntegerField(default=0)
+height=models.IntegerField(default=0)
+occupation=models.CharField(max_length=200,blank=False,null=False)
+
