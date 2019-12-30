3f3f6ca2c2a3e3ec87a4fe19fefcf4a829ef1a27
quangchien2682000 <quanglxph07563>
2019-12-30 13:44:44 +0700

    fix loi

--- apps/healthapp/models.py
+++ apps/healthapp/models.py
@@ -3,13 +3,12 @@ from django.db import models
 # Create your models here.
 
 
-class PostPrepayment(models.Model);
-
-email=models.CharField(max_length=200,blank=False,null=False)
-nameUser=models.CharField(max_length=200,blank=False,null=False)
-fullName=models.CharField(max_length=200,blank=False,null=False)
-birthDay=models.models.DateField(blank=False,null=False)
+class PostPrepayment(models.Model):
+email=models.CharField(max_length=255, blank=false, null=false)
+nameUser=models.CharField(max_length=255, blank=false, null=false)
+fullName=models.CharField(max_length=255, blank=false, null=false)
+birthDay=models.models.DateField(null=false)
 weight=models.IntegerField(default=0)
 height=models.IntegerField(default=0)
-occupation=models.CharField(max_length=200,blank=False,null=False)
+occupation=models.CharField(max_length=255,blank=false,null=false)
 
