936c3965059bb616ee6d7bc842d7bd6f2e7cbb50
lethi2k <ledinhthi2909@gmail.com>
2019-12-30 13:43:50 +0700

    update model

--- apps/healthapp/models.py
+++ apps/healthapp/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 
 
-class GetCompareHealth(models.Models):
-    images = models.CharField(max_length=255,blank=false,null=false)
-    product_name = models.TextField(max_length=255,blank=false,null=false)
+class ProductCompareHealth(models.Models):
+    images = models.CharField(max_length=255)
+    product_name = models.TextField(max_length=255)
     cover = models.IntegerField(default=0)
     features = models.IntegerField(default=0)
     premium = models.IntegerField(default=0)
